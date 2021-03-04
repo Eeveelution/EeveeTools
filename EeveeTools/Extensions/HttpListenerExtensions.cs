a3226fb7b3120e680ffd5433df5660fc7607629b
Eeveelution <developerbrzoza@gmail.com>
2021-03-04 13:22:15 +0100

    another extension to httplistenerresponse

--- EeveeTools/Extensions/HttpListenerExtensions.cs
+++ EeveeTools/Extensions/HttpListenerExtensions.cs
@@ -4,11 +4,31 @@ using System.Threading.Tasks;
 
 namespace EeveeTools.Extensions {
     public static class HttpListenerExtensions {
+        /// <summary>
+        /// Writes a String and Closes the Connection
+        /// </summary>
+        /// <param name="Response">this Object</param>
+        /// <param name="String">What to Return</param>
+        /// <returns>awaitable Task</returns>
         public static async Task WriteString(this HttpListenerResponse Response, string String) {
             //Convert String to byte[]
             byte[] originalString = Encoding.UTF8.GetBytes(String);
             //Write Async
             await Response.OutputStream.WriteAsync(originalString);
+            //Close Connection
+            Response.Close();
+        }
+        /// <summary>
+        /// Sends a Byte Array of Data and Closes the Connection
+        /// </summary>
+        /// <param name="Response">this Object</param>
+        /// <param name="Data">What to send Back</param>
+        /// <returns>awaitable Task</returns>
+        public static async Task WriteBytes(this HttpListenerResponse Response, byte[] Data) {
+            //Write Async
+            await Response.OutputStream.WriteAsync(Data);
+            //Close Connection
+            Response.Close();
         }
     }
 }

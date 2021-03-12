d4581e539d8ebb5cea6ee6cec1517c8222122d50
Eeveelution <developerbrzoza@gmail.com>
2021-03-13 00:27:47 +0100

    http test

--- EeveeTools.TestApp/Program.cs
+++ EeveeTools.TestApp/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using EeveeTools.Extensions;
+using EeveeTools.Servers.HTTP;
 using EeveeTools.Servers.TCP;
 
 namespace EeveeTools.TestApp {
     class Program {
         static void Main(string[] args) {
-            TcpServer server = new("127.0.0.1", 13382, typeof(Handler));
+            AsyncHttpServer server = new("http://127.0.0.1:80/",
+            async (S, Context) => {
+                await Context.Response.WriteString("Hello!");
+            });
             server.Start();
+
             Console.ReadLine();
         }
     }

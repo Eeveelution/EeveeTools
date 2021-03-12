97ee9fd8e38db20bea4e59d5ae9d34739aed0aa8
Eeveelution <developerbrzoza@gmail.com>
2021-03-12 22:01:17 +0100

    Comments

--- EeveeTools/Database/DatabaseHandler.cs
+++ EeveeTools/Database/DatabaseHandler.cs
@@ -6,30 +6,32 @@ using MySqlConnector;
 
 namespace EeveeTools.Database {
     public class DatabaseHandler {
-        public static async Task<IReadOnlyDictionary<string, object>> QueryAsync(string Query, MySqlParameter[] Parameters, DatabaseContext Context) {
+        public static async Task<IReadOnlyDictionary<string, object>> QueryAsync(DatabaseContext Context, string Query, MySqlParameter[] Parameters = null) {
+            //This is where results will be stored
             Dictionary<string, object> results = new();
-
+            //Creates a new Connection
             await using MySqlConnection connection = new(Context.GetConnectionString());
-
+            //Creates a Command and assings the Command Text to desired Query
             MySqlCommand command = connection.CreateCommand();
             command.CommandText = Query;
-
+            //Add Parameters if there arent any
             if(Parameters != null)
                 command.Parameters.AddRange(Parameters);
-
+            //Create Reader and get Schema
             await using MySqlDataReader reader = await command.ExecuteReaderAsync();
             ReadOnlyCollection<DbColumn> columns = await reader.GetColumnSchemaAsync();
-
+            //Read Results
             while (await reader.NextResultAsync()) {
+                //Get Values
                 object[] values = new object[columns.Count];
                 reader.GetValues(values);
-
+                //For all Values, add results
                 for (int i = 0; i != columns.Count; i++) {
                     DbColumn column = columns[i];
                     results.Add(column.ColumnName, values[i]);
                 }
             }
-
+            //Return Results
             return results;
         }
     }

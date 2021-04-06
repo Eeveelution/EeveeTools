b25678fec40125b2da9aa1706eb99ea776e3ee6c
Eeveelution <developerbrzoza@gmail.com>
2021-04-06 23:15:08 +0200

    async and sync

--- EeveeTools/Extensions/HttpListenerExtensions.cs
+++ EeveeTools/Extensions/HttpListenerExtensions.cs
@@ -10,11 +10,25 @@ namespace EeveeTools.Extensions {
         /// <param name="response">this Object</param>
         /// <param name="string">What to Return</param>
         /// <returns>awaitable Task</returns>
-        public static async Task WriteString(this HttpListenerResponse response, string @string) {
+        public static async Task WriteStringAsync(this HttpListenerResponse response, string @string) {
             //Convert String to byte[]
             byte[] originalString = Encoding.UTF8.GetBytes(@string);
             //Write Async
             await response.OutputStream.WriteAsync(originalString);
+            //Close Connection
+            response.Close();
+        }
+        /// <summary>
+        /// Writes a String and Closes the Connection
+        /// </summary>
+        /// <param name="response">this Object</param>
+        /// <param name="string">What to Return</param>
+        /// <returns>awaitable Task</returns>
+        public static void WriteString(this HttpListenerResponse response, string @string) {
+            //Convert String to byte[]
+            byte[] originalString = Encoding.UTF8.GetBytes(@string);
+            //Write Async
+            response.OutputStream.Write(originalString);
             //Close Connection
             response.Close();
         }
@@ -24,9 +38,20 @@ namespace EeveeTools.Extensions {
         /// <param name="response">this Object</param>
         /// <param name="data">What to send Back</param>
         /// <returns>awaitable Task</returns>
-        public static async Task WriteBytes(this HttpListenerResponse response, byte[] data) {
+        public static async Task WriteBytesAsync(this HttpListenerResponse response, byte[] data) {
             //Write Async
             await response.OutputStream.WriteAsync(data);
+            //Close Connection
+            response.Close();
+        }
+        /// <summary>
+        /// Sends a Byte Array of Data and Closes the Connection
+        /// </summary>
+        /// <param name="response">this Object</param>
+        /// <param name="data">What to send Back</param>
+        public static void WriteBytes(this HttpListenerResponse response, byte[] data) {
+            //Write
+            response.OutputStream.Write(data);
             //Close Connection
             response.Close();
         }

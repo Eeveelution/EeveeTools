8127c0b36cd0b954f9cbc798f93c357f620821fc
Eeveelution <developerbrzoza@gmail.com>
2021-04-06 23:18:59 +0200

    sync variants for the database handler

--- EeveeTools/Database/DatabaseHandler.cs
+++ EeveeTools/Database/DatabaseHandler.cs
@@ -54,6 +54,74 @@ namespace EeveeTools.Database {
             return results.ToArray();
         }
         /// <summary>
+        /// Executes a Query
+        /// </summary>
+        /// <param name="context">Database Context</param>
+        /// <param name="query">SQL</param>
+        /// <param name="parameters">Parameters</param>
+        public static IReadOnlyDictionary<string, object>[] Query(DatabaseContext context, string query, MySqlParameter[] parameters = null) {
+            //Place to store Results
+            List<IReadOnlyDictionary<string, object>> results = new();
+            //Create and Open Connection
+            MySqlConnection connection = new(context.GetConnectionString());
+            connection.Open();
+            //Create command with `query` Command Text
+            MySqlCommand cmd = connection.CreateCommand();
+            cmd.CommandText = query;
+            //Add Parameters if present
+            if(parameters != null)
+                cmd.Parameters.AddRange(parameters);
+            //Execute Data Reader
+            using MySqlDataReader reader = cmd.ExecuteReader();
+            //Read Columns
+            ReadOnlyCollection<DbColumn> columns = reader.GetColumnSchema();
+            //Read Results
+            while (reader.Read()) {
+                //Place to store Result
+                Dictionary<string, object> result = new();
+                //Create Object Array with the size of the amount of columns
+                object[] values = new object[columns.Count];
+                //Get Values
+                reader.GetValues(values);
+                //Iterate over each column
+                for (int i = 0; i < columns.Count; i++) {
+                    //Get Column
+                    DbColumn column = columns[i];
+                    //Add Result
+                    result.Add(column.ColumnName, values[i]);
+                }
+                //Add Result to Results
+                results.Add(result);
+            }
+            //Close Connection
+            connection.Close();
+            //Return Results
+            return results.ToArray();
+        }
+        /// <summary>
+        /// Executes a Non Query
+        /// </summary>
+        /// <param name="context">Database Context</param>
+        /// <param name="query">SQL</param>
+        /// <param name="parameters">Parameters</param>
+        /// <returns>awaitable Task</returns>
+        public static void NonQuery(DatabaseContext context, string query, MySqlParameter[] parameters = null) {
+            //Creates a new Connection
+            using MySqlConnection connection = new(context.GetConnectionString());
+            //Open Connection
+            connection.Open();
+            //Creates a Command and assings the Command Text to desired Query
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = query;
+            //Add Parameters if there arent any
+            if(parameters != null)
+                command.Parameters.AddRange(parameters);
+            //Execute Non Query
+            command.ExecuteNonQuery();
+            //Close Connection
+            connection.Close();
+        }
+        /// <summary>
         /// Executes a Non Query Asynchonously
         /// </summary>
         /// <param name="context">Database Context</param>
